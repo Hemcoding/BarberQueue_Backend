8ca30ad0e54b357c38a90eae77f2898110271829
Hemanshu Parmar <hemanshup2002@gmail.com>
2024-05-15 11:10:45 +0530

    env config changed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import { app } from "./app.js";
 
+dotenv.config({
+    path: "./env",
+});
 // const app = express()
 
 const port = process.env.PORT || 8000;
 
-dotenv.config({
-    path: "./env",
-});
 
 connectDB()
 .then(()=>{
